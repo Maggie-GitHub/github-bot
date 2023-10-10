18e8cb7e359e54c1412168cab80247781f8cae52
manyu <manyu14630@gmail.com>
2023-10-09 21:40:14 -0700

    random number
    random for x(0, 54), y for (0, 6), loop n = 100 times

--- index.js
+++ index.js
@@ -2,19 +2,51 @@ const jsonfile = require('jsonfile');
 const moment = require('moment');
 const simpleGit = require('simple-git');
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 const FILE_PATH = './data.json';
 
-const makeCommit = (x, y) =>{
+const makeCommit = n =>{
+   if(n===0) return simpleGit().push();
+   const x = getRandomInt(54);
+   const y = getRandomInt(6);
+   const DATE = moment().subtract(1,'y').add(1, 'd')
+       .add(x, 'w').add(y, 'd').format();
+
+   const data = {
+       date: DATE
+   }
+   console.log(DATE);
+
+   jsonfile.writeFile(FILE_PATH, data, ()=>{
+       simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE},
+           makeCommit.bind(this, --n));
+   });
+}
+
+makeCommit(100);
+
+/**const FILE_PATH = './data.json';
+
+const makeCommit = n =>{
+    if(n===0) return simpleGit().push();
+    const x = random.int(0, 54);
+    const y = random.int(0, 6);
     const DATE = moment().subtract(1,'y').add(1, 'd')
         .add(x, 'w').add(y, 'd').format();
 
     const data = {
         date: DATE
     }
+    console.log(DATE);
 
     jsonfile.writeFile(FILE_PATH, data, ()=>{
-        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
+        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE},
+            makeCommit.bind(this, --n));
     });
 }
 
-makeCommit(3, 3)
+makeCommit(100);
+ */
